b99c220573686ff5e892a539f5622b3aaee3fa32
TechNikunj <technikunj@gmail.com>
2016-09-27 16:36:57 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -3,7 +3,6 @@
 #mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #mysock.connect(('www.google.co.in', 80))
 #mysock.send('GET http://www.google.co.in HTTP/1.0\n\n')
-
 #while True:
 #	data = mysock.recv(512)
 #	if ( len(data) < 1 ):
